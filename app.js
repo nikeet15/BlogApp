007573198ffa4fb361fcfcc1a21f4a1059619626
nikeet15 <kesharinikeet@gmail.com>
2020-04-04 20:16:24 +0530

    provided backend support to edit blog page

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));                  //making public a static directory
 app.set("view engine", "ejs");                      //if written no need to write .ejs only write name of file
 
+var methodOverride= require("method-override");
+app.use(methodOverride("_method"));
+
 var mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
     if (!err)
@@ -19,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true
         console.log("error in DB cnnection" + err);
 });
 
-//model schema.............
+//mongoose model schema.............
 var blogSchema= mongoose.Schema({
     title: String,
     image: String,
@@ -28,7 +31,6 @@ var blogSchema= mongoose.Schema({
 });
 
 var Blog= mongoose.model("blog", blogSchema);
-
 
 //routes..............
 app.get("/", function (req, res) {
@@ -70,29 +72,41 @@ app.post("/blogs", function(req, res){
 app.get("/blogs/:id", function(req, res){
     Blog.findById(req.params.id, function (err, foundblog) {
         if (!err) {
-            console.log("3.Blog found");
+            console.log("3. Blog found");
             console.log(foundblog);
             res.render("show", { blog: foundblog });
         }
         else{
-            console.log("3.Blog not found");
+            console.log("3. Blog not found");
         }
     });
 });
 
- app.get("/blogs/:id/edit", function(req, res){
-     Blog.findById(req.params.id, function (err, foundblog) {
-         if (!err) {
-             console.log("3.Blog found");
-             console.log(foundblog);
-             res.render("edit", { blog: foundblog });
-         }
-         else {
-             console.log("3.Blog not found");
-         }
-     });
- });
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (!err) {
+            console.log("4.edit Blog found");
+            console.log(foundBlog);
+            res.render("edit", { blog: foundBlog });
+        }
+        else {
+            console.log("4. edit Blog not found");
+        }
+    });
+});
 
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (!err) {
+            console.log("5.Blog updated");
+            console.log(req.body.blog);
+            res.redirect("/blogs/"+req.params.id);
+        }
+        else {
+            console.log("5. Problem in updating blog");
+        }
+    })
+});
 
 
  
